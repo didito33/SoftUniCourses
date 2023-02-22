415f59fa0f398c45edabf7bc0871273323430aed
didito33 <didito331324@gmail.com>
2023-02-22 13:03:36 +0200

    Intro

---
+++ ASP.NET-Fundamentals/01.ASP-Introduction/WebShopDemo.Core/Data/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopDemo.Core.Data.Models
+{
+    [Comment("Products to sell")]
+    public class Product
+    {
+        [Key]
+        [Comment("Primary key")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Comment("Product name")]
+        public string Name { get; set; }
+
+        [Comment("Product price")]
+        [Required]
+        public decimal Price { get; set; }
+
+        [Comment("Products in stock")]
+        [Required]
+        public int Quantity { get; set; }
+
+        [Comment("Product status")]
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ ASP.NET-Fundamentals/01.ASP-Introduction/WebShopDemo.Core/Models/ProductDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopDemo.Core.Models
+{
+    /// <summary>
+    /// Product model
+    /// </summary>
+    public class ProductDto
+    {
+        /// <summary>
+        /// Product identifier
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Product name
+        /// </summary>
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Product price
+        /// </summary>
+        [Range(0.1, (double)decimal.MaxValue, ConvertValueInInvariantCulture = true)]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Propduct quantity in stock
+        /// </summary>
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}

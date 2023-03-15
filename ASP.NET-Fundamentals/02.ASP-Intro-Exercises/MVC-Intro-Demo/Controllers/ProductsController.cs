4d520a47c81e2886b0216ec5a82a2c062e734e11
didito33 <didito331324@gmail.com>
2023-03-16 00:12:00 +0200

    ASP.NET

---
+++ ASP.NET-Fundamentals/02.ASP-Intro-Exercises/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Text.Json;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products
+            = new List<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Id = 1,
+                    Name = "Cheese",
+                    Price = 7.00
+                },
+                new ProductViewModel()
+                {
+                    Id = 2,
+                    Name = "Ham",
+                    Price = 5.50
+                },
+                new ProductViewModel()
+                {
+                    Id = 3,
+                    Name = "Bread",
+                    Price = 1.50
+                }
+            };
+
+        public IActionResult ById(int id)
+        {
+            var product = this.products
+                .FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            return Json(products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            var text = string.Empty;
+            foreach (var product in products)
+            {
+                text += $"Product {product.Id}: {product.Name} - {product.Price}lv";
+                text += "\r\n";
+            }
+            return Content(text);
+        }
+
+        //Wrong screenshot and not stated in the exercises doc
+        //public IActionResult AllAsTextFile()
+        //{
+        //    return View(this.products);
+        //}
+
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword)
+        {
+            if (keyword != null)
+            {
+                var foundProducts = this.products
+                    .Where(pr => pr.Name.ToLower()
+                    .Contains(keyword.ToLower()));
+
+                return View(foundProducts);
+            }
+
+            return View(this.products);
+        }
+    }
+}
